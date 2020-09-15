056f84c29fccfa88fea68b3894ed2182b58c841f
laurapease <laurapeasetm@gmail.com>
2020-09-14 21:28:57 -0500

    Commit 6 - Yell at the Ninja Turtles answered

--- scripts/app.js
+++ scripts/app.js
@@ -94,3 +94,18 @@ console.log(jimHaff);
 let removed = jimHaff.splice(2, 1);
 
 console.log(jimHaff);
+
+// ## Yell at the Ninja Turtles
+// 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+
+// 2. Use a `for loop` to call `.toUpperCase()` on each of them and print out the result.
+
+// 3. Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
+
+let ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
+
+for (i = 0; i < ninjaTurtles.length; i++) {
+  ninjaTurtles[i] = ninjaTurtles[i].toUpperCase();
+}
+
+console.log(ninjaTurtles);
