069b72ca2b41cc04cab53a75bb8e88940b6971d0
laurapease <laurapeasetm@gmail.com>
2020-09-14 21:08:03 -0500

    Commit 5 - Getting to Know You answered

--- scripts/app.js
+++ scripts/app.js
@@ -18,6 +18,12 @@ for (let i = 0; i <= 200; i++) {
 // 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
 // 2. For every ** even ** number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
 
+/////I THINK THIS NEEDS AN ARRAY OF RESPONSES AND A WHILE LOOP....COME BACK TO THIS
+
+//for (let i = 1; i < 21; i++) {
+//if (i )
+//}
+
 // ## Fizz Buzz
 
 // Write a javascript application that logs all numbers from 1 - 100 ** AND **:
@@ -34,3 +40,57 @@ for (let i = 1; i < 101; i++) {
   else if (i % 5 === 0 && i % 3 === 0) console.log("FizzBuzz");
   else console.log(i);
 }
+
+// ## Getting to Know You
+// Use the following arrays to answer the questions below (name, age, hometown):
+// ```javascript
+// const kenny = ["Kenny", 1000, "Austin"];
+// const jimHaff = ["Jim H", 16, "All cities"];
+// const reuben = ["Reuben", 22, "Durham"];
+// const jimClark = ["Jim C", 186, "LA"];
+// const ryan = ["Ryan", 65, "Denver"];
+// ```
+// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the `kenny` array and replace it with "Gameboy".
+
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older.  Don't just hard code `187`--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+
+// 3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+// 4. Reuben left Durham 5 years ago to come to Chicago. First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+
+// 5. Jim Haff could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but [see if you can do it in one line of code](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push)
+
+// 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities.  Whatever the _first_ of the 3 cities for Jim Haff is now, remove it from the array using [`.splice()`]
+
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+
+kenny[0] = "Gameboy";
+
+console.log(kenny);
+
+jimClark[1] = jimClark[1] + 1;
+
+console.log(jimClark);
+
+ryan[2] = "Gotham City";
+
+console.log(ryan);
+
+reuben.pop();
+reuben.push("Chicago");
+
+console.log(reuben);
+
+jimHaff.pop();
+
+jimHaff.push("Nashville", "Austin", "Seattle");
+
+console.log(jimHaff);
+
+let removed = jimHaff.splice(2, 1);
+
+console.log(jimHaff);
